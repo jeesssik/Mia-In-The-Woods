b954ec53c0fccc74af21416ba177e47846f45b10
jeesssik <jeesssik@gmail.com>
2025-11-06 08:58:07 -0300

    GameManager added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        // Patrón Singleton básico
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        Debug.Log("Juego iniciado");
+    }
+}
